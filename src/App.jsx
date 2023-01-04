0d65766e33021d0469e3e926bba30ea7ca9a4f74
matiasortubia <matiortubia@gmail.com>
2023-01-04 02:21:42 -0300

    Add: Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components';
+import { Home } from './pages';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={ <h1>Home</h1> } />
-        <Route path="/home" element={ <h1>Home</h1> } />
-        <Route path="/index" element={ <h1>Home</h1> } />
-        <Route path="/projects" element={ <h1>Projects</h1> } />
-        <Route path="/contact" element={ <h1>Contact</h1> } />
-        <Route path="*" element={ <h1>404 Not Found</h1> } />
-      </Routes>
-      <Navbar />
-    </Router>
+    <div className="container">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={ <Home /> } />
+          <Route path="/home" element={ <Home /> } />
+          <Route path="/index" element={ <Home /> } />
+          <Route path="/projects" element={ <h1>Projects</h1> } />
+          <Route path="/contact" element={ <h1>Contact</h1> } />
+          <Route path="*" element={ <h1>404 Not Found</h1> } />
+        </Routes>
+        <Navbar />
+      </Router>
+    </div>
   )
 }
 
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import styles from './home.module.css';
+
+const Home = () => {
+    return (
+        <section className={ styles.home }>
+            <div className={ styles.titleContainer }>
+                <h1 className={ styles.name }>
+                    Hi! I'm Matías
+                </h1>
+                <h2 className={ styles.title }>
+                    Web Developer
+                </h2>
+            </div>
+        </section>
+    );
+};
+
+export { Home };
