c7f41638023478093490580e691edf195e2335f7
matiasortubia <matiortubia@gmail.com>
2023-02-24 17:30:15 -0300

    Add theme context

---
+++ src/context/ThemeContextWrapper.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { ThemeContext } from './ThemeContext';
+
+export const ThemeContextWrapper = ({children}) => {
+    const [darkMode, setDarkMode] = useState(true);
+
+    function toggleDarkMode() {
+        setDarkMode(prevTheme => !prevTheme);
+    }
+
+    return (
+        <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
+            { children }
+        </ThemeContext.Provider>
+    );
+};
