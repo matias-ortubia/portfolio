19d72cde8c78aa3b83c6623a1db9af19caa0baac
matiasortubia <matiortubia@gmail.com>
2023-03-08 04:26:05 -0300

    Add socials on home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import { LinkedinIcon, TwitterIcon, GithubIcon } from '../../components';
 import styles from './home.module.css';
 
 const Home = () => {
@@ -10,6 +11,19 @@ const Home = () => {
                 <h2 className={ styles.title }>
                     Web Developer
                 </h2>
+            </div>
+            <div class={ styles.socialsContainer }>
+                <div className={ styles.line } />
+                    <a href="https://github.com/matiasortubia/" target="_blank">
+                        <GithubIcon className="color-icon-secondary" size="4rem" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/matiasortubia/" target="_blank">
+                        <LinkedinIcon className="color-icon-secondary" size="4rem" />
+                    </a>
+                    <a href="https://twitter.com/MatiRtb" target="_blank">
+                        <TwitterIcon className="color-icon-secondary" size="4rem" />
+                    </a>
+                <div className={ styles.line } />
             </div>
         </section>
     );
